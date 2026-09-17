675dab1dbe658429515168b7cacfb6dd5cf9e898
iNoma <702sunnylife@gmail.com>
2026-09-18 06:52:52 +0900

    Bump android-app versionCode/versionName to 0.2

--- android-app/build.gradle.kts
+++ android-app/build.gradle.kts
@@ -48,8 +48,8 @@ android {
         applicationId = "com.smilestudio.android"
         minSdk = libs.versions.androidMinSdk.get().toInt()
         targetSdk = libs.versions.androidTargetSdk.get().toInt()
-        versionCode = 1
-        versionName = "0.1"
+        versionCode = 2
+        versionName = "0.2"
 
         buildConfigField(
             "String",
